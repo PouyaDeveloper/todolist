7b38533116475aa2b0989d3a4cda8018645f9f36
user1 <pooya_srfm@.yahoo.com>
2020-08-10 21:58:39 +0430

    add newyear url

--- program/program/urls.py
+++ program/program/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/',include("hello.urls"))
+    path('hello/',include("hello.urls")),
+    path('newyear/',include("newyear.urls"))
 
 ]
