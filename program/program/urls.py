bdefeafd240e56c11a2e3b13b593007a042f98ec
user1 <pooya_srfm@.yahoo.com>
2020-08-11 22:44:29 +0430

    tasks urls

--- program/program/urls.py
+++ program/program/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',include("hello.urls")),
-    path('newyear/',include("newyear.urls"))
+    path('newyear/',include("newyear.urls")),
+    path('tasks/', include("tasks.urls"))
 
 ]
