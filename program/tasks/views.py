a5206c541958e64ad686d6097d30cf00fd12dcd4
user1 <pooya_srfm@.yahoo.com>
2020-08-11 23:10:10 +0430

    task index function

--- program/tasks/views.py
+++ program/tasks/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+tasks=["practicing", "study", "jerking"]
+
+def index(request):
+    return render(request,"tasks/index.html", {"tasks":tasks})
