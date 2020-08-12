828bd97711b19bba254909858ad39dd682b3a1a1
user1 <pooya_srfm@.yahoo.com>
2020-08-13 02:26:25 +0430

    adding task to tasks

--- program/tasks/views.py
+++ program/tasks/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
 tasks=["practicing", "study", "jerking"]
+
+class NewTaskForm(forms.Form):
+    task=forms.CharField(label="New Task")
+    
 
 def index(request):
     return render(request,"tasks/index.html", {"tasks":tasks})
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method=="POST":
+        form= NewTaskForm(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+    return render(request, "tasks/add.html", {"form":NewTaskForm()})
